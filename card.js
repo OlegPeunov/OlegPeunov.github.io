b0f8de053fc428e837615aad6cd43581ca2dfbac
Oleg Peunov <olegpeunov@yandex.ru>
2021-03-15 12:50:04 +0300

    revew comments deleted

--- card.js
+++ card.js
@@ -32,52 +32,11 @@ class Card{
           console.log(err)
         })
     } 
-    
-    /* Можно лучше:
-
-    Вынести удаление обработчиков с элементов в отдельный метод, например removeEventListeners и вызывать его внутри remove
-    */
-    
   }
 
 
   create=()=>{
-    /* Можно лучше:
-
-    Создавать карточку не вручную через createElement, а использовать
-    для этого разметку в виде шаблонной строки.
-
-    Стоит обратить внимание, что вставка данных с помощью интерполяции шаблонной строки и insertAdjacentHTML
-    может привести к уязвимости XSS, т.к. данные вставляются на страницу как обычный html, а если они придут
-    с сервера в данных может быть код злоумышленника и он будет вставлен на страницу как html и исполнится.
-    Поэтому необходимо фильтровать html теги во вставляемых данных (такая процедура называется HTML sanitization
-
-    Пример как это сделать есть здесь:
-    https://gomakethings.com/preventing-cross-site-scripting-attacks-when-using-innerhtml-in-vanilla-javascript/ )
-
-    или вставлять данные с помощью textContent и style.backgroundImage уже после создания разметки
-    элемента как показано на примере ниже:
-
-    const template = document.createElement("div");
-    template.insertAdjacentHTML('beforeend', `
-    <div class="place-card">
-        <div class="place-card__image">
-            <button class="place-card__delete-icon"></button>
-        </div>
-        <div class="place-card__description">
-            <h3 class="place-card__name"></h3>
-            <button class="place-card__like-icon"></button>
-        </div>
-    </div>`);
-    const placeCard = template.firstElementChild;
-    placeCard.querySelector(".place-card__name").textContent = name;
-    placeCard.querySelector(".place-card__image").style.backgroundImage = `url(${link})`;
-        
-        
-    Так же для создания разметки можно использовать тег tempate
-    https://learn.javascript.ru/template-tag
-    https://frontender.info/template/
-    */
+  
     const plaseCard = document.createElement('div');
     const plaseCardImage = document.createElement('div');
     const placeCardDeleteIcon = document.createElement('button');
@@ -96,9 +55,7 @@ class Card{
     placeCardlikesAmount.classList.add('place-card__likesAmount');
     
     placeCardName.textContent = this.cardData.name;
-    /*
-    Отлично, что используется интерполяция строк из ES6, так держать!
-    */
+
     plaseCardImage.style.backgroundImage = `url(${this.cardData.link})`
     placeCardLikeIcon.classList.add('place-card__like-icon');
 
@@ -126,7 +83,7 @@ class Card{
   _getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+    return Math.floor(Math.random() * (max - min)) + min; 
   }
 
 
